062ebe73ec1bed4a82db9cc05b9b369c6a2e4563
Sarah <sarahfong@outlook.fr>
2019-11-24 13:59:15 +0100

    Recup API

---
+++ TP4/routes/API_omdb.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+
+// Clé api
+const API_KEY = "3cd9444b";
+// Url API
+const API_URL = "http://www.omdbapi.com/";
+
+
+class API_omdb{
+    constructor(){
+    }
+
+    // Faire la requete à l'API omdb
+    // Retourne une promise
+    fetchMovieTitle(movieTitle){
+        return axios
+            .get(`${API_URL}?t=${movieTitle}&apikey=${API_KEY}`, {
+                crossdomain: true
+            })
+    }
+}
